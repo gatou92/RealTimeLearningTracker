99b8a05b81f9e828525d209e08bbacde224253ab
Maria <marakigt92@gmail.com>
2018-10-31 17:45:46 +0100

    first

--- Offline_Computation/run.py
+++ Offline_Computation/run.py
@@ -2,15 +2,11 @@
 
 #Run only this file that integrates all the procedures
 
-import sql2mongodb as sm
-import metrics_computation as mc
-import profiles_computation as pc
+import sql2mongodb 
+import metrics_computation 
+import profiles_computation 
 
 if __name__ == '__main__':
-
-    sm.func()
-    mc.func()
-    pc.func()
     print "All finished."
 
 
